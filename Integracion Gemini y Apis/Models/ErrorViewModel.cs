d065a7e64b004f78f2366079ff3c53ee8e46b66c
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-05-15 07:30:33 -0500

    Agregar archivos de proyecto.

---
+++ Integracion Gemini y Apis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Integracion_Gemini_y_Apis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Integracion Gemini y Apis/Models/GeminiModels.cs
@@ -0,0 +1,18 @@
+﻿namespace Integracion_Gemini_y_Apis.Models
+{
+    public class GeminiModels
+    {
+        public class  GeminiRequest 
+        {
+            public List<Content> contents { get; set; }
+        }
+        public class GeminiContent
+        {
+            public List<GeminiRequest> parts { get; set; }
+        }
+        public class GeminiPart
+        {
+            public string text { get; set; }
+        }
+    }
+}
